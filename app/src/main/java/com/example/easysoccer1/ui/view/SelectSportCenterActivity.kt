a04b5e8aabf5b3850906d57daead9186b3ec93e8
WILMANYHS17 <whernandezsuesca@gmail.com>
2023-05-19 14:41:32 -0500

    SelectSportCenter

---
+++ app/src/main/java/com/example/easysoccer1/ui/view/SelectSportCenterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.easysoccer1.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectSportCenterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_sport_center)
+    }
+}
---
+++ app/src/main/java/com/example/easysoccer1/ui/viewmodel/SelectSportCenterViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.easysoccer1.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.easysoccer1.domain.SportCenterUseCase
+
+class SelectSportCenterViewModel(
+    private val sportCenterUseCase: SportCenterUseCase
+) : ViewModel() {
+
+
+
+}
