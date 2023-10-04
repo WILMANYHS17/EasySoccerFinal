14585f849db6e705534741697748386b5b564fc4
WILMANYHS17 <whernandezsuesca@gmail.com>
2023-10-04 09:24:19 -0500

    Ventana Stats, muestra los días. Las imágenes 100% se van a cargar y se regresaron a los tipos de datos originales

---
+++ app/src/test/java/com/example/easysoccer1/data/database/DataBaseImplTest.kt
@@ -0,0 +1,122 @@
+package com.example.easysoccer1.data.database
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import org.junit.jupiter.api.Assertions.*
+
+class DataBaseImplTest {
+    private val dataBase: FirebaseFirestore = TODO()
+    private val dataBaseStorage: FirebaseStorage = TODO()
+
+    @org.junit.jupiter.api.Test
+    fun createUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setImageUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getImageUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getUserComplete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun searchUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun changePassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun createSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setImageSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getImageSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setListImageSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListImageSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getNitSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListSportCenter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setGoals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListGoals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun deleteGoal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListReserveAdmin() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getSportCenterUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListSportsCenterUsers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setReserve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListReserveUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun cancelReserve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getGoal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun updateGoal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setComment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getListComments() {
+    }
+}
